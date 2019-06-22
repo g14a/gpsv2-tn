b505c999a2ed93a8504d526c2c1a3a67697daef7
g14a <gowtham.m81197@gmail.com>
2019-06-22 14:28:10 +0530

    merging dev.
    Signed-off-by: g14a <gowtham.m81197@gmail.com>

--- errorcheck/error_check.go
+++ errorcheck/error_check.go
@@ -1,7 +1,3 @@
-<<<<<<< HEAD:errorcheck/error_check.go
-// Package errorcheck just checks if an error occurs and logs it on stdout
-=======
->>>>>>> dev:errcheck/error_check.go
 package errorcheck
 
 import "log"
